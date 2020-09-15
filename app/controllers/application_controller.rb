12b831c22f7a93e11f86da04f923b913a7023487
Beglov Sergey <beglov.sergey@yandex.ru>
2020-09-15 16:22:12 +0300

    Client and employee vue packs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_client!
-
-  layout :layout
-
-  private
-
-  def layout
-    is_a?(Devise::SessionsController) ? 'logon' : 'application'
-  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def js_pack
+    if current_client
+      'client'
+    elsif current_employee
+      'employee'
+    else
+      'application'
+    end
+  end
 end
